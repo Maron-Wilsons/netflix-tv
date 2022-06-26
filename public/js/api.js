7a339cdc60bf3df444ad773080662e68f52c8b30
Maron-Wilsons <96543124+Maron-Wilsons@users.noreply.github.com>
2022-06-26 12:03:18 +0300

    NETFLIX

---
+++ public/js/api.js
@@ -0,0 +1,8 @@
+let api_key = "d32d3ce52356a23b585608eb96a0980d";
+
+let img_url = "https://image.tmdb.org/t/p/w500";
+let original_img_url = "https://image.tmdb.org/t/p/original";
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?";
+
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?"; 
+let movie_datail_http = "https://api.themoviedb.org/3/movie/";
